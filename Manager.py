f16a69a22f2a77c2d2c40114ba25999d2c7ae96a
Gabriel <tetradog@gmail.com>
2021-08-15 13:26:58 +0200

    commit

--- Manager.py
+++ Manager.py
@@ -1,19 +1,20 @@
 import MultiAccount
+import json
 
 class Manager:
 
-    def __init__(self,folderDownload=None,folderDownloadBackUps=None):
+    def __init__(self,folderDownload=None,folderDownloadBackups=None):
         self.Accounts=MultiAccount();
-        self.BackUps=MultiAccount();
+        self.Backups=MultiAccount();
         if folderDownload is not None:
             self.Accounts.Downloads=folderDownload;
-        if folderDownloadBackUps is not None:
-            self.BackUps.Downloads=folderDownloadBackUps;
+        if folderDownloadBackups is not None:
+            self.Backups.Downloads=folderDownloadBackups;
     
     def Upload(self,fileName,folderMega=None,fileMega=None,removeFileDisk=False):
         result=self.Accounts.Upload(fileName,folderMega,fileMega,removeFileDisk);
         if result is not None:
-            result=self.BackUps.Upload(fileName,folderMega,fileMega,removeFileDisk);
+            result=self.Backups.Upload(fileName,folderMega,fileMega,removeFileDisk);
             isOk=result is not None;
         else:
             isOk=False;
@@ -24,10 +25,26 @@ class Manager:
         for file in self.Accounts.GetFiles():
             dicFiles[file]=file;
         
-        for file in self.BackUps.GetFiles():
+        for file in self.Backups.GetFiles():
             if file not in dicFiles:
                 yield file;
     
     def RestoreNotFound(self):
         for file in self.GetFileNotFound():
             self.Accounts.Upload(file);
+
+
+    def CredentialsToJSon(self):
+        credentials={
+            'Accounts':self.Accounts.LoginCredentials,
+            'Backups':self.Backups.LoginCredentials
+
+        };
+        return json.dump(credentials);
+    def LoadCredentials(self,credentialsJsonString):
+        credentials=json.loads(credentialsJsonString);
+        for email,password in credentials['Accounts']:
+            self.Accounts.AddOrReplace(email,password);
+        for email,password in credentials['Backups']:
+            self.Backups.AddOrReplace(email,password);
+
